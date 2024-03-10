ee39efbca2b2264a8968223924b89336c9f9cb7d
LOSTSOUL2104 <113973970+LOSTSOUL2104@users.noreply.github.com>
2024-03-10 09:58:22 +0530

    app.js

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
-
-
 const wrapper = document.querySelector(".sliderWrapper");
 const menuItems = document.querySelectorAll(".menuItem");
-
 const products = [
   {
     id: 1,
     title: "Air Force",
-    price: 119,
+ 
     colors: [
       {
         code: "black",
@@ -21,8 +18,7 @@ const products = [
   },
   {
     id: 2,
-    title: "Air Jordan",
-    price: 149,
+    title: "Nocta",
     colors: [
       {
         code: "lightgray",
@@ -37,7 +33,7 @@ const products = [
   {
     id: 3,
     title: "Blazer",
-    price: 109,
+
     colors: [
       {
         code: "lightgray",
@@ -52,7 +48,7 @@ const products = [
   {
     id: 4,
     title: "Crater",
-    price: 129,
+  
     colors: [
       {
         code: "black",
@@ -67,7 +63,7 @@ const products = [
   {
     id: 5,
     title: "Hippie",
-    price: 99,
+  
     colors: [
       {
         code: "gray",
@@ -85,7 +81,7 @@ let choosenProduct = products[0];
 
 const currentProductImg = document.querySelector(".productImg");
 const currentProductTitle = document.querySelector(".productTitle");
-const currentProductPrice = document.querySelector(".productPrice");
+
 const currentProductColors = document.querySelectorAll(".color");
 const currentProductSizes = document.querySelectorAll(".size");
 
@@ -99,12 +95,28 @@ menuItems.forEach((item, index) => {
 
     //change texts of currentProduct
     currentProductTitle.textContent = choosenProduct.title;
-    currentProductPrice.textContent = "$" + choosenProduct.price;
     currentProductImg.src = choosenProduct.colors[0].img;
 
     //assing new colors
     currentProductColors.forEach((color, index) => {
       color.style.backgroundColor = choosenProduct.colors[index].code;
     });
+  });
+});
+
+currentProductColors.forEach((color, index) => {
+  color.addEventListener("click", () => {
+    currentProductImg.src = choosenProduct.colors[index].img;
+  });
+});
+
+currentProductSizes.forEach((size, index) => {
+  size.addEventListener("click", () => {
+    currentProductSizes.forEach((size) => {
+      size.style.backgroundColor = "white";
+      size.style.color = "black";
+    });
+    size.style.backgroundColor = "black";
+    size.style.color = "white";
   });
 });
